8689229e1552164af9a1b66de684e3d6bf1ab63a
shradhaagarwal01 <62115289+shradhaagarwal01@users.noreply.github.com>
2021-09-29 16:17:27 +0530

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,16 +5,16 @@ function About(props) {
     return (
         <div id="About" className="about">
             <h2>A LITTLE BIT ABOUT ME</h2>
-            <div className="intro">
-                <img className = "profile" src={Shradha} alt="profile"/>
-                <p>Hey! My name is Shradha Agarwal and I'm a web developer with a passion for front end development and design.
-                I'm currently a final year student at Jaypee University of Information Technology pursuing Bachelor's degree in Computer Science.
-                I aspire toward a career that will allow me to channel my creativity through crafting beautiful software and engaging experiences.</p>
-            </div>
-            <div className="button-box">
-                <button><a href="#Projects">See Projects</a></button>
-                <button><a href="mailto:shradhaagarwal386@gmail.com">Hire Me</a></button>
-            </div>
+                <div className="intro">
+                    <img className = "profile" src={Shradha} alt="profile"/>
+                    <p>Hey! My name is Shradha Agarwal and I'm a web developer with a passion for front end development and design.
+                    I'm currently a final year student at Jaypee University of Information Technology pursuing Bachelor's degree in Computer Science.
+                    I aspire toward a career that will allow me to channel my creativity through crafting beautiful software and engaging experiences.</p>
+                </div>
+                <div className="button-box">
+                    <button><a href="#Projects">See Projects</a></button>
+                    <button><a href="mailto:shradhaagarwal386@gmail.com">Hire Me</a></button>
+                </div>
         </div>
     );
 }
